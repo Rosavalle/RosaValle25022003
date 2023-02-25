daeb64ab8d7f8a672cbdf31e667945d4a858ce1e
Rosavalle <125718156+Rosavalle@users.noreply.github.com>
2023-02-24 22:56:03 -0600

    Creando programa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// See https: //aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, Word!"):
+
+Console.Write("ingresar el valor inicial: ");
+            int valorInicial = int.Parse(Console.ReadLine());
+
+            Console.Write("ingresar el valor final: ");
+                         int valorFinal = int.Parse(Console.ReadLine());
+
+                         Console.Write("Los números pares dentro del rango ingresado son: ");
+
+                         for (int i = valorInicial; i <= valorFinal; i++)
+                         {
+                             if (i % 2 == 0)
+                          {
+                            Console.Write(i + " ");
+                          }
+                         }
